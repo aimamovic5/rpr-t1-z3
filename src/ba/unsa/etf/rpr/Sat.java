8a15d937c595246271b70f5e88935561e54a1ec5
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-27 02:00:27 +0200

    Dodane metode Sljedeci i Prethodni

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -14,5 +14,28 @@ public class Sat {
         this.minute = minute;
         this.sekunde = sekunde;
     }
-
-}
+    public void Sljedeci() {
+        this.sekunde++;
+        if (this.sekunde == 60) {
+            this.sekunde = 0; this.minute++;
+        }
+        if (this.minute == 60) {
+            this.minute = 0; this.sati++;
+        }
+        if (this.sati == 24) {
+            this.sati = 0;
+        }
+    }
+    public void Prethodni() {
+        this.sekunde--;
+        if (this.sekunde == -1) {
+            this.sekunde = 59; this.minute--;
+        }
+        if (this.minute == -1) {
+            this.minute = 59; this.sati--;
+        }
+        if (this.sati == -1) {
+            this.sati = 23;
+        }
+    }
+   }
